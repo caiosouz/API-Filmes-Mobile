a497ca7e2ec9eb9ab53284d183e068332fdf09d2
Caio de Souza <caio-de-souza@hotmail.com>
2023-09-04 22:03:43 -0300

    Projeto De Filmes

--- services/apiFilmes.jsx
+++ services/apiFilmes.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzgwMTVjZTNkOTM0YjJmZGMxNGE5YjMxYWM2ZmQ2YiIsInN1YiI6IjY0MzQ4ZWE2ZTkyZDgzMDA3N2Y5M2I4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZCyzohkoCufKB-aj635WvHK8qilik_FDYAgmXu0HbnE'
+        Authorization: 'Bearer'
     }
 })
 
